4cbf6cb8fe7cb35831ded0e91648889cd3b1a86c
Tayo <jubriltayo@gmail.com>
2024-07-09 10:31:51 +0100

    host added

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'vercel.app', 'now.sh']
+ALLOWED_HOSTS = ['127.0.0.1', 'vercel.app', 'now.sh', 'userorgmanagement-8xxrpmvei-tayo-jubrils-projects.vercel.app']
 
 
 # Application definition
